29dbca25726c25ea6c07a90f9ce5ec41a378445f
JelleDekkers <jelle.dekkers@hotmail.com>
2019-08-27 19:46:36 +0200

    Added rsp file to ignore incorrect 'variable not used' warnings

---
+++ Assets/Scripts/UI/LoginScreen.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginScreen : MonoBehaviour
+{
+    [SerializeField] private Button loginButton;
+
+    private LoginService loginService;
+
+    public void InjectDependencies(LoginService loginService)
+    {
+        this.loginService = loginService;
+    }
+
+    private void OnEnable()
+    {
+        loginButton.onClick.AddListener(OnLoginButtonClickedEvent);
+    }
+
+    private void OnDisable()
+    {
+        loginButton.onClick.RemoveListener(OnLoginButtonClickedEvent);
+    }
+
+    private void OnLoginButtonClickedEvent()
+    {
+        loginService.Login();
+    }
+}
