d3082218c6253513a0f353d009e93322cb3272b6
JelleDekkers <jelle.dekkers@hotmail.com>
2020-02-04 16:29:22 +0100

    Added test flow machine

---
+++ Assets/Scripts/TestAction.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Utils.Core.Flow;
+
+public class TestAction : StateAction
+{
+    [SerializeField] private float testValue;
+
+    public override void OnStarting() { }
+
+    public override void OnStarted() { }
+
+    public override void Update() { }
+
+    public override void OnStopping() { }
+
+    public override void OnStopped() { }
+}
---
+++ Assets/Scripts/TestRule.cs
@@ -0,0 +1,11 @@
+﻿using Utils.Core.Flow;
+
+public class TestRule : Rule
+{
+    public override string DisplayName => "On ";
+    public override bool IsValid => base.IsValid;
+
+    public override void OnActivate() { }
+
+    public override void OnDeactivate() { }
+}
